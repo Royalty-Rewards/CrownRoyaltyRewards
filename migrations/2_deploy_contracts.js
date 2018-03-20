040e0752ea874e940db5428f1d43f0f6adea38ab
Grattan Bodkin <gbodkin@uaudio.com>
2018-03-20 16:45:58 -0700

    add deployment script for crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,45 +1,69 @@
-// const config = require('../truffle.js');
-module.exports = function (deployer) {
-  // deployer.deploy(Ownable);
+const config = require('../truffle-config.js');
+require('babel-register');
+require('babel-polyfill');
+
+var CRWNRR_ReferralBasedCrowdsale = artifacts.require("./CRWNRR_ReferralBasedCrowdsale.sol");
+var CRWNRR_Token = artifacts.require("./CRWNRR_Token.sol");
+var SplitPayment = artifacts.require("./SplitPayment.sol");
+
+
+
+module.exports = function (deployer, network, accounts) {
+  deployContracts(deployer, accounts);
 };
-// var CRWNRR_ReferralBasedCrowdsale = artifacts.require("./CRWNRR_ReferralBasedCrowdsale.sol");
-// var CRWNRR_Token = artifacts.require("./CRWNRR_Token.sol");
-// var SplitPayment = artifacts.require("./SplitPayment.sol");
-//
-//
-//
-// module.exports = function (deployer, accounts) {
-//   const accounts = web3.eth.accounts;
-//   const start = web3.eth.getBlock('latest').timestamp;
-//   const finish = start + (84600 * 30);
-//   const cap = ether(8888);
-//   const lessThanCap = ether(60);
-//   const initialRate = new BigNumber(9166);
-//   const finalRate = new BigNumber(5500);
-//   const minimumPurchase = ether(1);
-//
-//   return deployer.deploy(CRWNRR_Token, accounts[0]).then(() =>{
-//     return CRWNRR_Token.deployed().then(async CRWNRR_TokenInstance => {
-//       return deployer.deploy(SplitPayment, [accounts[1],accounts[2]], [15,15], {from: owner}).then(() => {
-//         return SplitPayment.deployed().then(async SplitPaymentInstance => {
-//           return deployer.deploy(CRWNRR_ReferralBasedCrowdsale, start, finish, initialRate, finalRate, cap, SplitPaymentInstance.address, CRWNRR_TokenInstance.address, minimumPurchase, {from: owner}).then(() => {
-//             return CRWNRR_ReferralBasedCrowdsale.deployed().then(async crowdsaleInstance => {
-//                 await CRWNRR_TokenInstance.transferOwnership(crowdsaleInstance.address, {from: owner});
-//                 return deployer;
-//             });
-//           });
-//         });
-//       });
-//     });
-//   });
-// };
-//
-// function deployPresaleContracts()
-// {
-//
-// }
-//
-//
-// function ether (n) {
-//   return new web3.BigNumber(web3.toWei(n, 'ether'));
-// }
+
+function deployContracts(deployer, accounts)
+{
+  const owner = accounts[0];
+  const start = latestTime();
+  const finish = start + (84600 * 30); //30 days
+  const cap = web3.toWei("88888", "ether");
+  const initialRate = 9166;
+  const finalRate = 5500;
+  const minimumPurchase = web3.toWei("1", "ether");
+  return deployer
+      .then(() => {
+          return deployer.deploy(CRWNRR_Token)
+          .then(() => {
+            return CRWNRR_Token.deployed().then(async CRWNRR_TokenInstance => {
+              return deployer.deploy(
+                CRWNRR_ReferralBasedCrowdsale,
+                start,
+                finish,
+                initialRate,
+                finalRate,
+                cap,
+                accounts[3],
+                // SplitPaymentInstance.address,
+                CRWNRR_TokenInstance.address,
+                minimumPurchase,
+                {from: owner});
+              });
+          })
+      })
+      // .then(() => {
+      //     return deployer.deploy(
+      //       SplitPayment,
+      //       [accounts[1],accounts[2]],
+      //       [15,15],
+      //       {from: owner});
+      // })
+      // .then(() => {
+      //     return deployer.deploy(
+      //       CRWNRR_ReferralBasedCrowdsale,
+      //       start,
+      //       finish,
+      //       initialRate,
+      //       finalRate,
+      //       cap,
+      //       accounts[3],
+      //       // SplitPaymentInstance.address,
+      //       CRWNRR_TokenInstance.address,
+      //       minimumPurchase,
+      //       {from: owner});
+      // });
+}
+
+function latestTime() {
+  return web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1
+}
